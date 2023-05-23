1cd82e19a50c32a50e188de15ecaba5de0abd259
alexakbar <alexampp11@gmail.com>
2023-05-23 12:13:44 +0700

    api without login

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'v1'], function () {
     Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register'])->name('register');
     Route::post('login', [App\Http\Controllers\API\AuthController::class, 'login'])->name('login');
+
+    Route::get('categories', [App\Http\Controllers\API\CategoryController::class, 'getCategories'])->name('getCategories');
+    Route::get('sources', [App\Http\Controllers\API\SourceController::class, 'getSources'])->name('getSources');
+    Route::get('authors', [App\Http\Controllers\API\AuthorController::class, 'getAuthors'])->name('getAuthors');
+
     Route::group(['middleware' => 'auth:api'], function () {
         Route::get('logout', [App\Http\Controllers\API\AuthController::class, 'logout'])->name('logout');
         Route::get('user', [App\Http\Controllers\API\UserController::class, 'getUser'])->name('getUser');
-        Route::get('categories', [App\Http\Controllers\API\CategoryController::class, 'getCategories'])->name('getCategories');
-        Route::get('authors', [App\Http\Controllers\API\AuthorController::class, 'getAuthors'])->name('getAuthors');
         Route::post('set-personalize', [App\Http\Controllers\API\UserController::class, 'setPersonalize'])->name('setPersonalize');
         Route::post('search-articles', [App\Http\Controllers\API\ArticlesController::class, 'searchArticles'])->name('searchArticles');
     });
